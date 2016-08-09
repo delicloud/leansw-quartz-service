a47cbf6f48ebc310abe7683bcdf744cff1f7f085
YongliuLi <ylli@thoughtworks.com>
2016-08-09 10:47:42 +0800

    [#0] add index redirect

--- src/main/java/com/thoughtworks/lean/quartz/QuartzService.java
+++ src/main/java/com/thoughtworks/lean/quartz/QuartzService.java
@@ -3,6 +3,12 @@ package com.thoughtworks.lean.quartz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by yongliuli on 7/27/16.
@@ -10,7 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Controller
 public class QuartzService {
+    @RequestMapping("/")
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/ui/quartz/jobs");
+    }
     public static void main(String[] args) {
         SpringApplication.run(QuartzService.class, args);
     }
